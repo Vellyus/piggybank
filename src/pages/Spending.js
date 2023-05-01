7078b052a2d5fb19d23c52e8a3361ea18c55f663
Vellyus <noisyboy026@gmail.com>
2023-05-01 15:45:30 +0200

    put back Spending.js

---
+++ src/pages/Spending.js
@@ -0,0 +1,10 @@
+import { NavLink, useParams } from "react-router-dom"
+
+export function Spending() {
+  const { id } = useParams()
+  const dbUrl = "https://piggy-bank-7eaa5-default-rtdb.europe-west1.firebasedatabase.app/"
+
+  return (
+    <li> <NavLink to={ `${ dbUrl }${ id }.json` }>{ id }</NavLink></li>
+  )
+}
