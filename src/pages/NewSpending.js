92dab1a7b4a0ec1f939d16745b5c116c554aac0a
Vellyus <noisyboy026@gmail.com>
2023-05-02 17:05:01 +0200

    added new spending form html + styles

--- src/pages/NewSpending.js
+++ src/pages/NewSpending.js
@@ -2,6 +2,20 @@ export function NewSpending() {
   return (
     <main>
       <h2>Új költés</h2>
+
+      <form id="newSpendingForm" action="submit-form.php" method="post">
+        <label for="item">Tétel:
+          <input type="text" id="item" name="item"></input></label>
+
+        <label for="date">Dátum:
+          <input type="text" id="date" name="date"></input></label>
+
+        <label for="amount">Összeg:
+          <input type="text" id="amount" name="amount"></input></label>
+
+
+        <button type="submit" value="Submit">Mentés</button>
+      </form>
     </main>
   )
 }
