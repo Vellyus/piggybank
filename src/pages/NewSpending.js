b227fcd5aa0372e8bdb6102e4ca8e8103974030c
Vellyus <noisyboy026@gmail.com>
2023-05-05 19:37:33 +0200

    changed css on newSpending

--- src/pages/NewSpending.js
+++ src/pages/NewSpending.js
@@ -5,13 +5,13 @@ export function NewSpending() {
 
       <form id="newSpendingForm" action="submit-form.php" method="post">
         <label for="item">Tétel:
-          <input type="text" id="item" name="item"></input></label>
+          <input type="text" id="item" name="item" placeholder="Piros zsiráf kerekeken"></input></label>
 
         <label for="date">Dátum:
-          <input type="date" id="date" name="date"></input></label>
+          <input type="date" id="date" name="date" placeholder="Dátum"></input></label>
 
         <label for="amount">Összeg:
-          <input type="number" id="amount" name="amount"></input></label>
+          <input type="number" id="amount" name="amount" placeholder="HUF"></input></label>
 
 
         <button type="submit" value="Submit">Mentés</button>
