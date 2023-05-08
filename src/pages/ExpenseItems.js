1ad69e0cea211a03af1ff563f9db1ff45462f4b5
Vellyus <noisyboy026@gmail.com>
2023-05-08 17:29:24 +0200

    changed tr element's key prop to the id

--- src/pages/ExpenseItems.js
+++ src/pages/ExpenseItems.js
@@ -18,9 +18,9 @@ export function ExpenseItems() {
         <th>Tétel</th>
         <th>Összeg</th>
         <th></th>
-        { data && Object.keys(data).map((e, i) => {
+        { data && Object.keys(data).map((e) => {
           return (<>
-            <tr key={ i } >
+            <tr key={ e } >
               <td>{ data[e].date }</td>
               <td>{ data[e].item } </td>
               <td>{ data[e].amount }</td>
