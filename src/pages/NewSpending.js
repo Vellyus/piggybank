ba8168a3134a130709fab0d2adaf4a6e81b8558b
Vellyus <noisyboy026@gmail.com>
2023-05-08 17:33:48 +0200

    changed for attributes to htmlFor

--- src/pages/NewSpending.js
+++ src/pages/NewSpending.js
@@ -4,13 +4,13 @@ export function NewSpending() {
       <h2>Új költés</h2>
 
       <form id="newSpendingForm" action="submit-form.php" method="post">
-        <label for="item">Tétel:
+        <label htmlFor="item">Tétel:
           <input type="text" id="item" name="item" placeholder="Piros zsiráf kerekeken"></input></label>
 
-        <label for="date">Dátum:
+        <label htmlFor="date">Dátum:
           <input type="date" id="date" name="date" placeholder="Dátum"></input></label>
 
-        <label for="amount">Összeg:
+        <label htmlFor="amount">Összeg:
           <input type="number" id="amount" name="amount" placeholder="HUF"></input></label>
 
 
