feeff88db959abaaa991df5e049f9e953fbb5833
Vellyus <noisyboy026@gmail.com>
2023-05-08 18:09:35 +0200

    added state to NewSpending

--- src/pages/NewSpending.js
+++ src/pages/NewSpending.js
@@ -1,11 +1,25 @@
+import { useState } from "react"
+
 export function NewSpending() {
+  const [formData, setFormData] = useState(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setFormData({
+      product: e.target.elements.product.value,
+      date: e.target.elements.date.value,
+      amount: e.target.elements.amount.value,
+    })
+  }
+
   return (
     <main>
       <h2>Új költés</h2>
 
-      <form id="newSpendingForm" action="submit-form.php" method="post">
-        <label htmlFor="item">Tétel:
-          <input type="text" id="item" name="item" placeholder="Piros zsiráf kerekeken"></input></label>
+      <form onSubmit={ (e) => handleSubmit(e) } id="newSpendingForm" action="submit-form.php" method="post">
+        <label htmlFor="product">Tétel:
+          <input type="text" id="product" name="product" placeholder="Piros zsiráf kerekeken"></input></label>
+
 
         <label htmlFor="date">Dátum:
           <input type="date" id="date" name="date" placeholder="Dátum"></input></label>
