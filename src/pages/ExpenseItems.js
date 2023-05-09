4f372dca3d8252b5e45fa6d178320d99469f4ad2
Vellyus <noisyboy026@gmail.com>
2023-05-09 15:45:10 +0200

    corrected table html

--- src/pages/ExpenseItems.js
+++ src/pages/ExpenseItems.js
@@ -14,21 +14,25 @@ export function ExpenseItems() {
   return (
     <>
       <table>
-        <th>Dátum</th>
-        <th>Tétel</th>
-        <th>Összeg</th>
-        <th></th>
-        { data && Object.keys(data).map((e) => {
-          return (<>
-            <tr key={ e } >
-              <td>{ data[e].date }</td>
-              <td>{ data[e].item } </td>
-              <td>{ data[e].amount }</td>
-              <td className="bold">Törlés | Módosítás</td>
-            </tr>
-          </>
-          )
-        }) }
+        <tbody>
+          <tr>
+            <th>Dátum</th>
+            <th>Tétel</th>
+            <th>Összeg</th>
+            <th></th>
+          </tr>
+          { data && Object.keys(data).map(e => {
+            return (<>
+              <tr key={ e } >
+                <td>{ data[e].date }</td>
+                <td>{ data[e].item } </td>
+                <td>{ data[e].amount }</td>
+                <td className="bold">Törlés | Módosítás</td>
+              </tr>
+            </>
+            )
+          }) }
+        </tbody>
       </table>
     </>
   )
