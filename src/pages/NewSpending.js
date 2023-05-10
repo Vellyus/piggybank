47208b79c69e8b54ee15437580a7654ff27beb80
Vellyus <noisyboy026@gmail.com>
2023-05-10 18:56:44 +0200

    added state to newSpendingForm

--- src/pages/NewSpending.js
+++ src/pages/NewSpending.js
@@ -1,14 +1,17 @@
 import { addNewSpending } from "../apiService"
+import { useState } from "react"
 
 export function NewSpending() {
+  let [formData, setFormData] = useState(null)
+
+  const handleInputChange = (e, key) => {
+    setFormData({ ...formData, [key]: e.target.value })
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault()
     try {
-      const product = e.target.elements.product.value
-      const date = e.target.elements.date.value
-      const amount = e.target.elements.amount.value
-
-      addNewSpending(crypto.randomUUID(), product, date, amount)
+      addNewSpending(crypto.randomUUID(), formData.product, formData.date, formData.amount)
       document.querySelector("#newSpendingForm").reset()
     } catch (error) {
       console.log(error)
@@ -19,16 +22,16 @@ export function NewSpending() {
     <main>
       <h2>Új költés</h2>
 
-      <form onSubmit={ (e) => handleSubmit(e) } id="newSpendingForm" action="submit-form.php" method="post">
+      <form onSubmit={ e => handleSubmit(e) } id="newSpendingForm" action="submit-form.php" method="post">
         <label htmlFor="product">Tétel:
-          <input type="text" id="product" name="product" placeholder="Piros zsiráf kerekeken" /></label>
+          <input onChange={ (e, key) => handleInputChange(e, key = "product") } type="text" id="product" name="product" placeholder="Piros zsiráf kerekeken" /></label>
 
 
         <label htmlFor="date">Dátum:
-          <input type="date" id="date" name="date" placeholder="Dátum" /></label>
+          <input onChange={ (e, key) => handleInputChange(e, key = "date") } type="date" id="date" name="date" placeholder="Dátum" /></label>
 
         <label htmlFor="amount">Összeg:
-          <input type="number" id="amount" name="amount" placeholder="HUF" /></label>
+          <input onChange={ (e, key) => handleInputChange(e, key = "amount") } type="number" id="amount" name="amount" placeholder="HUF" /></label>
 
 
         <button type="submit" value="Submit">Mentés</button>
