3ec62e79830ba86c2e356fdf951f06ec76efbfe5
Vellyus <noisyboy026@gmail.com>
2023-05-29 13:52:09 +0200

    changed img src again

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ export function Home() {
   return (
     <main>
       <h2>Főoldal - Válassz menüpontot</h2>
-      <img src="./img/piggy.jpg" alt="Money is falling around a piggy." />
+      <img src="img/piggy.jpg" alt="Money is falling around a piggy." />
     </main>
   )
 }
